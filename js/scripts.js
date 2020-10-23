4f010273cb06063305a7ad76dc4d4ab6acbb0519
taylulz <taylulzcode@gmail.com>
2020-10-23 14:02:43 -0700

    add images of each the appointed language to the bottom of the form. Make initially hidden in styles.css. Test first image reveal with .show() method in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
     const answer5 = $("input:radio[name=question5]:checked").val();
 
     // UI logic
-    $("#insertAnswer1").text(answer1)
-    $("#insertAnswer2").text(answer2)
-    $("#insertAnswer3").text(answer3)
-    $("#insertAnswer4").text(answer4)
-    $("#insertAnswer5").text(answer5)
+    if question1 + question2 + question3 + question 4 + question5 <= 5 {
+      $("#python").show();
+      $("#c#").hide();
+      $("#js").hide();
+      $("#rails").hide();
+    }
+    // $("#insertAnswer1").text(answer1)
+    // $("#insertAnswer2").text(answer2)
+    // $("#insertAnswer3").text(answer3)
+    // $("#insertAnswer4").text(answer4)
+    // $("#insertAnswer5").text(answer5)
 
     e.preventDefault();
     
