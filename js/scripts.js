05cd6306ef4c65e34271f72ca9dabc07990b4166
taylulz <taylulzcode@gmail.com>
2020-10-23 11:56:08 -0700

    add 2 more input fields and print the results below, inside an answer <p> tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("#survey").submit(function(e) {
     const answer1 = $("input#question1").val();
+    const answer2 = $("input#question2").val();
+    const answer3 = $("input#question3").val();
 
-    $("#results").text(answer1)
+    $("#insertAnswer1").text(answer1)
+    $("#insertAnswer2").text(answer2)
+    $("#insertAnswer3").text(answer3)
 
     e.preventDefault();
     
