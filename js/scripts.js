a46017d1cf9e446af3501e80721202d8f4dbe279
taylulz <taylulzcode@gmail.com>
2020-10-25 15:21:49 -0700

    Committing to save question: If my console is outputting the intended results, how come my conditions aren't showing the right pictures?

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,30 @@ $(document).ready(function() {
     const answer4 = parseInt($("input:radio[name=question4]:checked").val());
     const answer5 = parseInt($("input:radio[name=question5]:checked").val());
 
-    let combo = (answer1 + answer2 + answer3 + answer4 + answer5)
+    const combo = (answer1 + answer2 + answer3 + answer4 + answer5)
 
     console.log(combo)
 
     // UI logic
     if (combo <= 5) {
-      $("#python").show();
-      $("#rails").hide();
-      } else if (combo >= 7 && combo < 8) {
+        // $("#python").show();
+        // $("#rails").hide(); 
+        console.log("this is under 5")
+      } else if (combo >= 6 && combo < 9) {
         $("#csharp").show();
-      } else if (combo >= 8 && combo <= 14) {
+      } else if (combo >= 9 && combo <= 14) {
         $("#js").show();
-      } else (combo === 15 && combo > 6); {
+      } else (combo >= 15); {
         $("#rails").show();
       } 
         
     e.preventDefault();
   }) 
 })
+
+// what I'd like it to be:
+// if 0 (NaN): show "looks like ya missed somthin"
+// else if 1-5 ; show python
+// else if 6-8 ; show c#
+// else if 9-14 ; show js
+// else it is 15 ; show rails
