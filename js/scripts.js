8d2c45cbdad4fe89f7986ef312e5ed7bd4fcfee4
taylulz <taylulzcode@gmail.com>
2020-10-29 20:45:19 -0700

    fix branching error showing multiple images, hide results upon form re-submission, add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,27 @@
-// business logic
 $(document).ready(function() {
   $("#survey").submit(function(e) {
+
     const answer1 = parseInt($("input:radio[name=question1]:checked").val());
     const answer2 = parseInt($("input:radio[name=question2]:checked").val());
     const answer3 = parseInt($("input:radio[name=question3]:checked").val());
     const answer4 = parseInt($("input:radio[name=question4]:checked").val());
     const answer5 = parseInt($("input:radio[name=question5]:checked").val());
-    const combo = (answer1 + answer2 + answer3 + answer4 + answer5)
-    console.log(combo)
+    const combo = (answer1 + answer2 + answer3 + answer4 + answer5);
 
-    // UI logic
-    if (combo <= 5) {
+    $("#python, #csharp, #js, #rails").hide();
+    if (combo < 6) {
       $("#python").show();
-      console.log(combo)
     } else if (combo >= 6 && combo < 9) {
       $("#csharp").show();
-      console.log(combo)
     } else if (combo >= 9 && combo <= 14) {
       $("#js").show();
-      console.log(combo)
-    } else (combo === 15); {
+    } else if (combo === 0) {
       $("#rails").show();
-      console.log("this should not be here because it's not > 15")
-    } 
+    }
+      else {
+      alert("It looks like ya missed something!");
+    }
 
     e.preventDefault();
-    console.log();
-  }) 
-})
-
-// what I'd like it to be:
-// if 0 (NaN): show "looks like ya missed somthin"
-// else if 1-5 ; show python
-// else if 6-8 ; show c#
-// else if 9-14 ; show js
-// else it is 15 ; show rails
+  });
+});
